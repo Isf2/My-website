aeaf0a4d07914fe5241f3d59e98a69e73c5af709
Isf2 <98806016+Isf2@users.noreply.github.com>
2022-07-31 15:13:09 +0300

    coment the contant

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,9 +12,9 @@ function Footer() {
         <li><a href='#home'>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Expeirience</a></li>
-        <li><a href='#services'>Services</a></li>
-        <li><a href='#portfolio'>Portfolio</a></li>
-        <li><a href='#Testmonials'>Testmonials</a></li>
+        {/* <li><a href='#services'>Services</a></li> */}
+        {/* <li><a href='#portfolio'>Portfolio</a></li> */}
+        {/* <li><a href='#Testmonials'>Testmonials</a></li> */}
         <li><a href='#Contact'>Contact</a></li>
       </ul>
       <div className="footer__socials">
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,8 +12,8 @@ const Nav=() => {
     <nav>
       <a href='#home'  onClick={()=> setActiveNav('#home')} className={activeNav==='#home' ? 'active' : ''} ><AiOutlineHome/></a>
       <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav==='#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav==='#experience' ? 'active' : ''}><BiBook/></a>
-      <a href='#services'onClick={()=> setActiveNav('#services')} className={activeNav==='#services' ? 'active' : ''}><RiServiceLine/></a>
+      {/* <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav==='#experience' ? 'active' : ''}><BiBook/></a> */}
+      {/* <a href='#services'onClick={()=> setActiveNav('#services')} className={activeNav==='#services' ? 'active' : ''}><RiServiceLine/></a> */}
       <a href='#contact'onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact' ? 'active' : ''}><MdOutlineMessage/></a>
     </nav>
   )
