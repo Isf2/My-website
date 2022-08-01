30e7403e24dbfc08214d05a7d95d52f86170925e
Isf2 <98806016+Isf2@users.noreply.github.com>
2022-08-01 08:47:05 +0300

    switch

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,8 +4,9 @@ import {FaGithub} from "react-icons/fa"
  const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://github.com/Isf2' target="_blank"><BsLinkedin/></a>
-        <a href='https://www.linkedin.com/in/ibrahim-alsaif-7b3abb242/' target="_blank"><FaGithub/></a>
+     
+        <a href=' https://www.linkedin.com/in/ibrahim-alsaif-7b3abb242/' target="_blank"><BsLinkedin/></a>
+        <a href='https://github.com/Isf2' target="_blank"><FaGithub/></a>
     </div>
   )
 }
